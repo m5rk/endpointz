f67bd66d7f178a745695578f55c610f35d719fde
Mark McEahern <mark@mceahern.com>
2018-03-01 16:35:00 -0600

    Add the User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#email' do
+    context 'when there is a user' do
+      before { create :user }
+
+      it { is_expected.to validate_uniqueness_of(:email) }
+    end
+  end
+
+  describe '#password' do
+    it { is_expected.to have_valid(:password).when 'foo', 'bar' }
+    it { is_expected.not_to have_valid(:password).when nil, '' }
+  end
+
+  describe '#authenticate' do
+    context 'when there is a user' do
+      let(:user) { create :user, password: 'sekret' }
+
+      context 'and the password is correct' do
+        it 'is the user' do
+          expect(user.authenticate('sekret')).to eq(user)
+        end
+      end
+
+      context 'and the password is not correct' do
+        it 'is false' do
+          expect(user.authenticate('ruhroh')).to be_falsey
+        end
+      end
+    end
+  end
+end
