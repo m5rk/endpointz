c1e7aea3ca78ffadf977069ff9de74ba0749539a
Mark McEahern <mark@mceahern.com>
2018-03-02 15:36:06 -0600

    Add test for no playlists

--- spec/controllers/v1/playlists_controller_spec.rb
+++ spec/controllers/v1/playlists_controller_spec.rb
@@ -7,24 +7,43 @@ describe V1::PlaylistsController do
   describe 'GET #index' do
     before do
       request.env['HTTP_AUTHORIZATION'] = token
-      1.upto(5) do |n|
-        create :playlist, user: user, name: "Playlist #{n}"
-      end
     end
 
     let(:request_method) { get :index, params: params, xhr: true }
     let(:params) { { } }
 
-    it 'is :ok' do
-      request_method
+    context 'when the user has playlists' do
+      before do
+        1.upto(5) do |n|
+          create :playlist, user: user, name: "Playlist #{n}"
+        end
+      end
 
-      expect(response).to have_http_status(:ok)
+      it 'is :ok' do
+        request_method
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'contains the playlists' do
+        request_method
+
+        expect(json[:data]).to have(5).playlists
+      end
     end
 
-    it 'contains the playlists' do
-      request_method
+    context 'when the user does not have any playlists' do
+      it 'is :ok' do
+        request_method
 
-      expect(json[:data]).to have(5).playlists
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'does not contain any playlists' do
+        request_method
+
+        expect(json[:data]).to be_empty
+      end
     end
   end
 end
