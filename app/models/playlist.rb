2d5b85b30f30861e47b2454382b71f2e340150bb
Mark McEahern <mark@mceahern.com>
2018-03-15 10:40:09 -0700

    Add provisional import of Playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,4 +3,15 @@ class Playlist < ApplicationRecord
 
   has_many :playlist_songs
   has_many :songs, through: :playlist_songs
+
+  def self.import(user, data)
+    playlist = user.playlists.create
+
+    require 'csv'
+
+    CSV.parse(data, headers: true) do |row|
+      song = Song.where(name: row['song_name']).first!
+      playlist.songs << song
+    end
+  end
 end
--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -2,4 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Playlist, type: :model do
   it { is_expected.to belong_to(:user) }
+
+  describe '#import' do
+    context 'when there is an import' do
+      let(:import) do
+        <<~PLAYLIST
+song_name,song_artist
+Livin' On a Prayer,Bon Jovi
+Keep the Faith,Bon Jovi
+        PLAYLIST
+      end
+      let(:user) { create :user }
+      let(:action) { Playlist.import(user, import) }
+
+      before do
+        artist = create :artist, name: 'Bon Jovi'
+        song = create :song, name: "Livin' On a Prayer"
+        song.artists << artist
+
+        song = create :song, name: "Keep the Faith"
+        song.artists << artist
+      end
+
+      it 'adds the songs to the playlist' do
+        expect { action }.to change { user.playlists.count }.by(1)
+      end
+    end
+  end
 end
